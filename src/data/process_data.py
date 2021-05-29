2996004df592d190dcb60e7f89ded3db1db771e7
soldeace <barine@runbox.com>
2021-05-28 22:54:47 -0300

    feat: Implement load, clean, and save functions

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -1,17 +1,65 @@
 import sys
+import sqlalchemy
+import pandas as pd
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """Loads and transforms raw data.
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+
+    # concatenate the dataframes on the id
+    df = pd.merge(messages, categories, how="inner", on="id")
+
+    # expand the semi-collon categories into new columns
+    categories = df.categories.str.split(";", expand=True)
+
+    # extract a list of new column names for categories
+    row = categories.loc[0]
+    category_colnames = [x.split("-")[0] for x in row.values]
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    # convert category values to just numbers 0 or 1
+    # e.g. related-0 becomes 0, related-1 becomes 1
+    for column in categories:
+        categories[column] = categories[column].str.replace(f"{column}-", "")
+        categories[column] = categories[column].astype(int)
+
+    # drop the original categories column from `df`
+    df.drop("categories", axis=1, inplace=True)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.merge(df, categories, how="outer", left_index=True, right_index=True)
+
+    return df
 
 
 def clean_data(df):
-    pass
+    """Cleans data by removing duplicates and possible glitches from the original data set.
+    """
+
+    # drop duplicates
+    df.drop_duplicates(subset=None, keep="first", inplace=True)
+
+    # # drop rows where related = 2 (probably a glitch)
+    df = df[df["related"] != 2]
+
+    # drop "child_alone" category, there are no samples that fit this one
+    df.drop("child_alone", axis=1, inplace=True)
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """Dumps Pandas dataframe into an SQL database.
+    """
 
+    engine = create_engine(f'sqlite:///{database_filepath}')
+
+    df.to_sql('messages', engine, index=False, if_exists="replace")
 
 def main():
     if len(sys.argv) == 4:
